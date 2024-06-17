cea6c590471f2110d48b5a0ab4e9b1a3019a384c
arpita-2 <89323421+arpita-2@users.noreply.github.com>
2024-06-17 22:37:43 +0530

    area of triangle

--- main.js
+++ main.js
@@ -4,4 +4,15 @@ function calculateCircleArea() {
   const area = Math.PI * radius ** 2;
   document.getElementById("circleArea").textContent =
     "Area of the circle: " + area.toFixed(2);
+  //The parseFloat part of the code does this conversion from text to a number. Specifically, it looks at the
+  //text that the user typed into the box named "circleRadius" and tries to interpret it as a number.
+}
+
+//area of triangle
+function calculateTriangleArea() {
+  const breadth = parseFloat(document.getElementById("numberBreadth").value);
+  const height = parseFloat(document.getElementById("numberHeight").value);
+  const areaOfTriangle = 0.5 * breadth * height;
+  document.getElementById("TriangleArea").textContent =
+    "Area of Triangle: " + areaOfTriangle;
 }
